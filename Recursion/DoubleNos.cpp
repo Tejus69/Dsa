c1d68ae8b2981b0f9227986885dcee41bc1bde07
Tejus69 <tejshinde2005@gmail.com>
2025-01-07 10:23:58 +0530

    seocnd ques done

---
+++ Recursion/DoubleNos.cpp
@@ -0,0 +1,32 @@
+// double every no. from array 
+// find maximum number in array by recursion
+#include<iostream>
+
+using namespace std;
+
+void doubleNos(int arr[],  int size, int i){
+ if(i >= size){
+    return;
+ } 
+ // processing 
+   arr[i] = arr[i] * 2;
+
+  doubleNos(arr,size,i+1);
+
+}
+int main(){
+  int arr[5]={11,12,45,14,15};
+
+int size = 5;
+
+ doubleNos(arr,size,0);
+
+
+//printing by for each
+for(int num : arr){
+    cout<< num<< " ";
+}
+
+
+    return 0;
+}
